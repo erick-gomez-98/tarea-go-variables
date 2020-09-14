3d9f7d126cf1cca8bb622a24b616accea0c195f4
Erick Gómez <external.erickomar.gomezvalentin@mx.bosch.com>
2020-09-14 10:17:38 -0500

    Se agregó el código para calcular el área de un triángulo

--- area-triangulo.go
+++ area-triangulo.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	
+	var base, altura float64
+
+	fmt.Println("Calcular el área de un triangulo")
+	fmt.Println("Ingresa la base: ")
+	fmt.Scanf("%f", &base)
+	fmt.Println("Ingresa la altura: ")
+	fmt.Scanf("%f", &altura)
+	fmt.Printf("El area del triangulo es: %f \n", (base*altura)/2)
 }

614b63e6a54de36758712b75b311def74227fe6b
Erick Gómez <external.erickomar.gomezvalentin@mx.bosch.com>
2020-09-14 10:04:26 -0500

    Se agregó el código para calcular el area de un cuadrado

--- area-cuadrado.go
+++ area-cuadrado.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	
+	var lado int
+
+	fmt.Print("Calcular el área de un cuadrado\n Ingresa el tamaño de un lado del cuadrado: ")
+	fmt.Scanf("%d", &lado)
+	fmt.Printf("El area del cuadrado es: %d \n", lado*lado)
 }

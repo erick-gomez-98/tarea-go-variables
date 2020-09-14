206ffbd70feeff842bbdc348e4f186b9b513a4cc
Erick Gómez <external.erickomar.gomezvalentin@mx.bosch.com>
2020-09-14 10:39:21 -0500

    Se agregó el código para convertir de grados fahrenheit a grados celsius

--- convertir-f-c.go
+++ convertir-f-c.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	
+	var tempF float64
+
+	fmt.Print("Convertir de grados Fahrenheit a Celsius\n Ingresa la temperatura en Fahrenheit: ")
+	fmt.Scan(&tempF)
+	fmt.Printf("%f grados Fahrenheit son %f grados Celsius \n", tempF, (tempF-32)*5/9)
 }

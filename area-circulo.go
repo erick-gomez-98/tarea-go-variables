46e5a14b0613ccdd6efd7792fbd2c82d9901aeb6
Erick Gómez <external.erickomar.gomezvalentin@mx.bosch.com>
2020-09-14 10:27:09 -0500

    Se agregó el código para calcular el área de un círculo

--- area-circulo.go
+++ area-circulo.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	
+	var radio float64
+
+	fmt.Print("Calcular el área de un círculo\n Ingresa la medida del radio: ")
+	fmt.Scan(&radio)
+	fmt.Printf("El area del circulo es: %f \n", math.Pi*(math.Pow(radio, 2)))
 }
